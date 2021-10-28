7efb2ca8afbde3640dfed99d9fc647c5a7b69187
Laila Chammaa <laylachammaa@gmail.com>
2021-10-28 14:39:08 -0400

    working client and server

---
+++ CampusClientApplication/src/client/server/CampusID.java
@@ -0,0 +1,41 @@
+
+package client.server;
+
+
+import javax.xml.bind.annotation.XmlEnum;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for campusID.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * <p>
+ * <pre>
+ * &lt;simpleType name="campusID">
+ *   &lt;restriction base="{http://www.w3.org/2001/XMLSchema}string">
+ *     &lt;enumeration value="DVL"/>
+ *     &lt;enumeration value="KKL"/>
+ *     &lt;enumeration value="WST"/>
+ *   &lt;/restriction>
+ * &lt;/simpleType>
+ * </pre>
+ * 
+ */
+@XmlType(name = "campusID")
+@XmlEnum
+public enum CampusID {
+
+    DVL,
+    KKL,
+    WST;
+
+    public String value() {
+        return name();
+    }
+
+    public static CampusID fromValue(String v) {
+        return valueOf(v);
+    }
+
+}
---
+++ CampusClientApplication/src/client/server/Timeslot.java
@@ -0,0 +1,79 @@
+
+package client.server;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for timeslot complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="timeslot">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="end" type="{http://www.w3.org/2001/XMLSchema}float"/>
+ *         &lt;element name="start" type="{http://www.w3.org/2001/XMLSchema}float"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "timeslot", propOrder = {
+    "end",
+    "start"
+})
+public class Timeslot {
+
+    protected float end;
+    protected float start;
+
+    public Timeslot(float start, float end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    public Timeslot() {
+    }
+
+    /**
+     * Gets the value of the end property.
+     * 
+     */
+    public float getEnd() {
+        return end;
+    }
+
+    /**
+     * Sets the value of the end property.
+     * 
+     */
+    public void setEnd(float value) {
+        this.end = value;
+    }
+
+    /**
+     * Gets the value of the start property.
+     * 
+     */
+    public float getStart() {
+        return start;
+    }
+
+    /**
+     * Sets the value of the start property.
+     * 
+     */
+    public void setStart(float value) {
+        this.start = value;
+    }
+
+}
